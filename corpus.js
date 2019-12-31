04f50fe69b62b3be75a87c95cf86577230c08529
Subhajit Sahu <wolfram77@gmail.com>
2019-12-31 14:06:42 +0530

    corpus is generated

---
+++ corpus.js
@@ -0,0 +1,18 @@
+const CORPUS = new Map([
+  ["a", {abbr:"A.",lang:"Assami"}],
+  ["b", {abbr:"B.",lang:"Bengali"}],
+  ["g", {abbr:"G.",lang:"Gujarati"}],
+  ["h", {abbr:"H.",lang:"Hindi"}],
+  ["kan", {abbr:"Kan.",lang:"Kannada"}],
+  ["kash", {abbr:"Kash.",lang:"Kashmiri"}],
+  ["mal", {abbr:"Mal.",lang:"Malayalam"}],
+  ["mar", {abbr:"Mar.",lang:"Marathi"}],
+  ["o", {abbr:"O.",lang:"Oriya"}],
+  ["p", {abbr:"P.",lang:"Punjabi"}],
+  ["tam", {abbr:"Tam.",lang:"Tamil"}],
+  ["tel", {abbr:"Tel.",lang:"Telugu"}],
+  ["e", {abbr:"E.",lang:"English"}],
+  ["sci", {abbr:"Sci.",lang:"Scientific name"}],
+  ["common", {abbr:"Common",lang:"Common name"}],
+]);
+module.exports = CORPUS;
